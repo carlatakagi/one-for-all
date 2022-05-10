1548768382a8de979e822e080232fbffe095ab2b
carlatakagi <carlatakagi@gmail.com>
2022-05-09 22:46:06 -0400

    add req 2

---
+++ desafio2.sql
@@ -0,0 +1,5 @@
+SELECT
+  (SELECT COUNT(musica_id) FROM SpotifyClone.musicas) AS 'cancoes',
+  (SELECT COUNT(artista_id) FROM SpotifyClone.artista) AS 'artistas',
+  (SELECT COUNT(album_id) FROM SpotifyClone.album) AS 'albuns';
+ 
